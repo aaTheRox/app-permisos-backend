b9a6ab2e9264fa25a7bb5a55de4a9f4ddc37e92d
Sam Freeman <41429405+nouaryk@users.noreply.github.com>
2020-01-29 16:38:44 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,9 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://root:root@cluster0-pbpji.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  console.log('connected');
+  const collection = client.db("test").collection("users");
+  console.log(collection);
   // perform actions on the collection object
   client.close();
 });
