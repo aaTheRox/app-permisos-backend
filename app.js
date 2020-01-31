193a2df7a4ae4c2f241f0496f6c5420be709d249
Sam Freeman <41429405+nouaryk@users.noreply.github.com>
2020-01-31 14:12:07 +0100

    fuckedup cors

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-app.use(cors());
+
+//app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 app.listen(PORT, () => {
