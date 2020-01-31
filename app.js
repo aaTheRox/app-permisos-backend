f8fdea59bc5d89debd496437c037b7ba6d195694
Sam Freeman <41429405+nouaryk@users.noreply.github.com>
2020-01-31 14:15:13 +0100

    cors settings

--- app.js
+++ app.js
@@ -22,15 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-
-//app.use(cors());
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Node server running on http://localhost:${PORT}`);
